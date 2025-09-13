a07f15dd36aa5d527a761c39e94ecf07fb79c735
Kai Eberl <kai.eberl@tum.de>
2025-09-13 17:45:18 +0200

    fix

--- sources/arxiv/from_latex/get-data-hf.sh
+++ sources/arxiv/from_latex/get-data-hf.sh
@@ -4,4 +4,4 @@ set -e
 
 python hf-to-dolma.py
 python preprocess.py
-hf upload kai271/arxiv-papers-encoded data/arxiv/v0/documents . --repo-type dataset
+hf upload kai271/arxiv-papers-encoded data/arxiv/raw/documents . --repo-type dataset
